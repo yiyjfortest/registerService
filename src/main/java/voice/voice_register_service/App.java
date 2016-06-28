906d2468702f0c01de854b43cca73a9b01283393
Jea <18157142133@163.com>
2016-06-28 16:31:22 +0800

    commit

---
+++ src/main/java/voice/voice_register_service/App.java
@@ -0,0 +1,13 @@
+package voice.voice_register_service;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
